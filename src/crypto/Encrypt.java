7ac4b567e40e76b51db420683738a99cba32cc19
Hugues Devimeux <hugues.devimeux@gmail.com>
2020-10-22 22:02:39 +0200

    added xor cipher

--- src/crypto/Encrypt.java
+++ src/crypto/Encrypt.java
@@ -1,70 +1,74 @@
 package crypto;
 
 import java.util.Random;
+
 import java.lang.Math; // TODO Allowed ? 
 
 import static crypto.Helper.*;
 
 public class Encrypt {
-	
+
 	public static final int CAESAR = 0;
 	public static final int VIGENERE = 1;
 	public static final int XOR = 2;
 	public static final int ONETIME = 3;
-	public static final int CBC = 4; 
-	
+	public static final int CBC = 4;
+
 	public static final byte SPACE = 32;
-	
+
 	final static Random rand = new Random();
-	
-	//-----------------------General-------------------------
-	
+
+	// -----------------------General-------------------------
+
 	/**
-	 * General method to encode a message using a key, you can choose the method you want to use to encode.
-	 * @param message the message to encode already cleaned
-	 * @param key the key used to encode
-	 * @param type the method used to encode : 0 = Caesar, 1 = Vigenere, 2 = XOR, 3 = One time pad, 4 = CBC
+	 * General method to encode a message using a key, you can choose the method you
+	 * want to use to encode.
 	 * 
-	 * @return an encoded String
-	 * if the method is called with an unknown type of algorithm, it returns the original message
+	 * @param message the message to encode already cleaned
+	 * @param key     the key used to encode
+	 * @param type    the method used to encode : 0 = Caesar, 1 = Vigenere, 2 = XOR,
+	 *                3 = One time pad, 4 = CBC
+	 * 
+	 * @return an encoded String if the method is called with an unknown type of
+	 *         algorithm, it returns the original message
 	 */
 	public static String encrypt(String message, String key, int type) {
 		// TODO: COMPLETE THIS METHOD
-		
+
 		return null; // TODO: to be modified
 	}
-	
-	
-	//-----------------------Caesar-------------------------
-	
+
+	// -----------------------Caesar-------------------------
+
 	/**
-	 * Method to encode a byte array message using a single character key
-	 * the key is simply added to each byte of the original message
-	 * @param plainText The byte array representing the string to encode
-	 * @param key the byte corresponding to the char we use to shift
+	 * Method to encode a byte array message using a single character key the key is
+	 * simply added to each byte of the original message
+	 * 
+	 * @param plainText     The byte array representing the string to encode
+	 * @param key           the byte corresponding to the char we use to shift
 	 * @param spaceEncoding if false, then spaces are not encoded
 	 * @return an encoded byte array
 	 */
 	public static byte[] caesar(byte[] plainText, byte key, boolean spaceEncoding) {
-		assert(plainText != null);
+		assert (plainText != null);
 		// TODO: COMPLETE THIS METHOD
-		
+
 		return null; // TODO: to be modified
 	}
-	
+
 	/**
-	 * Method to encode a byte array message  using a single character key
-	 * the key is simply added  to each byte of the original message
-	 * spaces are not encoded
+	 * Method to encode a byte array message using a single character key the key is
+	 * simply added to each byte of the original message spaces are not encoded
+	 * 
 	 * @param plainText The byte array representing the string to encode
-	 * @param key the byte corresponding to the char we use to shift
+	 * @param key       the byte corresponding to the char we use to shift
 	 * @return an encoded byte array
 	 */
 	public static byte[] caesar(byte[] plainText, byte key) {
 		// TODO: COMPLETE THIS METHOD
 		return null; // TODO: to be modified
 	}
-	
+
 	// -----------------------XOR-------------------------
 
 	/**
@@ -76,9 +80,15 @@ public class Encrypt {
 	 * @return an encoded byte array
 	 */
 	public static byte[] xor(byte[] plainText, byte key, boolean spaceEncoding) {
+		assert plainText.length > 0;
+		// TODO ; Implement check for empty key ?
 		byte[] ciphered = new byte[plainText.length];
 		for (int i = 0; i < plainText.length; i++) {
-			ciphered[i] = (byte) (plainText[i] ^ key);
+			if (!(spaceEncoding) && plainText[i] == 32) {
+				ciphered[i] = (byte) 32;
+			} else {
+				ciphered[i] = (byte) (plainText[i] ^ key);
+			}
 		}
 		return ciphered;
 
@@ -94,72 +104,73 @@ public class Encrypt {
 	public static byte[] xor(byte[] plainText, byte key) {
 		return xor(plainText, key, false);
 	}
-	//-----------------------Vigenere-------------------------
-	
+	// -----------------------Vigenere-------------------------
+
 	/**
-	 * Method to encode a byte array using a byte array keyword
-	 * The keyword is repeated along the message to encode
-	 * The bytes of the keyword are added to those of the message to encode
-	 * @param plainText the byte array representing the message to encode
-	 * @param keyword the byte array representing the key used to perform the shift
+	 * Method to encode a byte array using a byte array keyword The keyword is
+	 * repeated along the message to encode The bytes of the keyword are added to
+	 * those of the message to encode
+	 * 
+	 * @param plainText     the byte array representing the message to encode
+	 * @param keyword       the byte array representing the key used to perform the
+	 *                      shift
 	 * @param spaceEncoding if false, then spaces are not encoded
-	 * @return an encoded byte array 
+	 * @return an encoded byte array
 	 */
 	public static byte[] vigenere(byte[] plainText, byte[] keyword, boolean spaceEncoding) {
-		// TODO: COMPLETE THIS METHOD		
+		// TODO: COMPLETE THIS METHOD
 		return null; // TODO: to be modified
 	}
-	
+
 	/**
-	 * Method to encode a byte array using a byte array keyword
-	 * The keyword is repeated along the message to encode
-	 * spaces are not encoded
-	 * The bytes of the keyword are added to those of the message to encode
+	 * Method to encode a byte array using a byte array keyword The keyword is
+	 * repeated along the message to encode spaces are not encoded The bytes of the
+	 * keyword are added to those of the message to encode
+	 * 
 	 * @param plainText the byte array representing the message to encode
-	 * @param keyword the byte array representing the key used to perform the shift
-	 * @return an encoded byte array 
+	 * @param keyword   the byte array representing the key used to perform the
+	 *                  shift
+	 * @return an encoded byte array
 	 */
 	public static byte[] vigenere(byte[] plainText, byte[] keyword) {
 		// TODO: COMPLETE THIS METHOD
 		return null; // TODO: to be modified
 	}
-	
-	
-	
-	//-----------------------One Time Pad-------------------------
-	
+
+	// -----------------------One Time Pad-------------------------
+
 	/**
-	 * Method to encode a byte array using a one time pad of the same length.
-	 *  The method  XOR them together.
+	 * Method to encode a byte array using a one time pad of the same length. The
+	 * method XOR them together.
+	 * 
 	 * @param plainText the byte array representing the string to encode
-	 * @param pad the one time pad
+	 * @param pad       the one time pad
 	 * @return an encoded byte array
 	 */
 	public static byte[] oneTimePad(byte[] plainText, byte[] pad) {
 		// TODO: COMPLETE THIS METHOD
 		return null; // TODO: to be modified
 	}
-	
-	
-	
-	
-	//-----------------------Basic CBC-------------------------
-	
+
+	// -----------------------Basic CBC-------------------------
+
 	/**
-	 * Method applying a basic chain block counter of XOR without encryption method. Encodes spaces.
+	 * Method applying a basic chain block counter of XOR without encryption method.
+	 * Encodes spaces.
+	 * 
 	 * @param plainText the byte array representing the string to encode
-	 * @param iv the pad of size BLOCKSIZE we use to start the chain encoding
+	 * @param iv        the pad of size BLOCKSIZE we use to start the chain encoding
 	 * @return an encoded byte array
 	 */
 	public static byte[] cbc(byte[] plainText, byte[] iv) {
 		// TODO: COMPLETE THIS METHOD
-		
+
 		return null; // TODO: to be modified
 	}
-	
-	
+
 	/**
 	 * Generate a random pad/IV of bytes to be used for encoding
+	 * 
 	 * @param size the size of the pad
 	 * @return random bytes in an array
 	 */
@@ -169,7 +180,5 @@ public class Encrypt {
 		return null; // TODO: to be modified
 
 	}
-	
-	
-	
+
 }
